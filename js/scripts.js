ac812248f2dc9ee2e9cd4ffce030a44a77032bf5
Dants96 <danieltutistar@gmail.com>
2020-09-08 15:01:16 -0500

    mejoras de aspecto y correccion de bug 0

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ const buscaminas = {
     aCamposMinas: []
 }
 
-function inicio(){
+function inicio() {
     buscaminas.numFilas = 10;
     buscaminas.numColumnas = 10;
     buscaminas.numMinasTotales = 12;
@@ -17,7 +17,7 @@ function inicio(){
     esparcirNumeros();
 }
 
-function pintarTablero(){
+function pintarTablero() {
     borrarTablero();
 
     let tablero = document.querySelector("#tablero");
@@ -26,8 +26,8 @@ function pintarTablero(){
     document.querySelector("html").style.setProperty("--num-filas", buscaminas.numFilas);
     document.querySelector("html").style.setProperty("--num-columnas", buscaminas.numColumnas);
 
-    for(let i=0; i<buscaminas.numFilas; i++){
-        for(let j=0; j<buscaminas.numColumnas; j++){
+    for (let i = 0; i < buscaminas.numFilas; i++) {
+        for (let j = 0; j < buscaminas.numColumnas; j++) {
             let newDiv = document.createElement("div");
             newDiv.setAttribute("id", "f" + i + "_c" + j);
             newDiv.dataset.fila = i;
@@ -40,45 +40,48 @@ function pintarTablero(){
     }
 }
 
-function borrarTablero(){
+function borrarTablero() {
     let tablero = document.querySelector("#tablero");
-    while(tablero.firstChild){
+    while (tablero.firstChild) {
         tablero.firstChild.removeEventListener("contextmenu", marcar);
         tablero.firstChild.removeEventListener("click", destapar);
         tablero.removeChild(tablero.firstChild);
     }
 }
 
-function marcar(miEvento){
-    if(miEvento.type === "contextmenu"){
+function marcar(miEvento) {
+    if (miEvento.type === "contextmenu") {
         console.log(miEvento);
-        let casilla =miEvento.currentTarget;
+        let casilla = miEvento.currentTarget;
         miEvento.stopPropagation();
         miEvento.preventDefault();
 
         let fila = casilla.dataset.fila;
         let columna = casilla.dataset.columna;
 
-        if(fila>=0 && columna>=0 && fila<buscaminas.numFilas && columna<buscaminas.numColumnas){
-            if(casilla.classList.contains("icon-bandera")){
-                casilla.classList.remove("icon-bandera");
-                casilla.classList.add("icon-duda");
-                buscaminas.numMinasEncontradas --;
-            }else if(casilla.classList.contains("icon-duda")){
-                casilla.classList.remove("icon-duda");
-            }else if(casilla.classList.length == 0){
-                casilla.classList.add("icon-bandera");
+        if (fila >= 0 && columna >= 0 && fila < buscaminas.numFilas && columna < buscaminas.numColumnas) {
+            if (casilla.classList.contains("fa-flag")) {
+                casilla.classList.remove("fa-flag");
+                casilla.classList.add("fa-question");
+                buscaminas.numMinasEncontradas--;
+            } else if (casilla.classList.contains("fa-question")) {
+                casilla.classList.remove("fa-question");
+                casilla.classList.remove("fas");
+            } else if (casilla.classList.length == 0) {
+                casilla.classList.add("fas");
+                casilla.classList.add("fa-flag");
                 buscaminas.numMinasEncontradas++;
-                if(buscaminas.numMinasEncontradas == buscaminas.numMinasTotales){
-                    buscaminas.resolverTablero(true);
+                if (buscaminas.numMinasEncontradas == buscaminas.numMinasTotales) {
+                    resolverTablero(true);
                 }
             }
         }
     }
 }
 
-function destapar(miEvento){
-    if(miEvento.type === "click"){
+
+function destapar(miEvento) {
+    if (miEvento.type === "click") {
         let casilla = miEvento.currentTarget;
         let fila = casilla.dataset.fila;
         let columna = casilla.dataset.columna;
@@ -87,32 +90,36 @@ function destapar(miEvento){
     }
 }
 
-function destaparCasilla(fila, columna){
-    console.log("destapando casilla ["+fila+"]["+columna+"]");
+function destaparCasilla(fila, columna) {
+    fila = parseInt(fila);
+    columna = parseInt(columna);
+    console.log("destapando casilla [" + fila + "][" + columna + "]");
 
-    if (fila >-1 && fila < buscaminas.numFilas && columna >-1 && columna < buscaminas.numColumnas){
+
+    if (fila > -1 && fila < buscaminas.numFilas && columna > -1 && columna < buscaminas.numColumnas) {
         let casilla = document.querySelector("#f" + fila + "_c" + columna);
-        if(!casilla.classList.contains("destapado")){
-            if(!casilla.classList.contains("icon-bandera")){
+        if (!casilla.classList.contains("destapado")) {
+            if (!casilla.classList.contains("fa-flag")) {
                 casilla.classList.add("destapado");
                 casilla.innerHTML = buscaminas.aCamposMinas[fila][columna];
                 casilla.classList.add("c" + buscaminas.aCamposMinas[fila][columna]);
-                if(buscaminas.aCamposMinas[fila][columna] !== "B"){
-                    if(buscaminas.aCamposMinas[fila][columna] == 0){
-                        destaparCasilla(fila-1, columna-1);
-                        destaparCasilla(fila-1, columna);
-                        destaparCasilla(fila-1, columna+1);
-                        destaparCasilla(fila, columna-1);
-                        destaparCasilla(fila, columna+1);
-                        destaparCasilla(fila+1, columna-1);
-                        destaparCasilla(fila+1, columna);
-                        destaparCasilla(fila+1, columna+1);
+                if (buscaminas.aCamposMinas[fila][columna] !== "B") {
+                    if (buscaminas.aCamposMinas[fila][columna] == 0) {
+                        destaparCasilla(fila - 1, columna - 1);
+                        destaparCasilla(fila - 1, columna);
+                        destaparCasilla(fila - 1, columna + 1);
+                        destaparCasilla(fila, columna - 1);
+                        destaparCasilla(fila, columna + 1);
+                        destaparCasilla(fila + 1, columna - 1);
+                        destaparCasilla(fila + 1, columna);
+                        destaparCasilla(fila + 1, columna + 1);
 
                         casilla.innerHTML = "";
                     }
-                }else if(buscaminas.aCamposMinas[fila][columna] == "B"){
+                } else if (buscaminas.aCamposMinas[fila][columna] == "B") {
                     casilla.innerHTML = "";
-                    casilla.classList.add("icon-bomba");
+                    casilla.classList.add("fas");
+                    casilla.classList.add("fa-bomb");
                     casilla.classList.add("sinmaracar");
                     resolverTablero(false);
                 }
@@ -122,37 +129,37 @@ function destaparCasilla(fila, columna){
 }
 
 //generar matriz del juego 
-function generarCampoMinasVacio(){
+function generarCampoMinasVacio() {
     buscaminas.aCamposMinas = new Array(buscaminas.numFilas);
-    for(let fila=0; fila<buscaminas.numFilas; fila++){
+    for (let fila = 0; fila < buscaminas.numFilas; fila++) {
         buscaminas.aCamposMinas[fila] = new Array(buscaminas.numColumnas);
     }
 }
 
-function esparcirMinas(){
+function esparcirMinas() {
     let numMinasEsparcidas = 0;
-    while(numMinasEsparcidas < buscaminas.numMinasTotales){
+    while (numMinasEsparcidas < buscaminas.numMinasTotales) {
 
-        let fila     = Math.floor(Math.random() * buscaminas.numFilas);
-        let columna  = Math.floor(Math.random() * buscaminas.numColumnas);
+        let fila = Math.floor(Math.random() * buscaminas.numFilas);
+        let columna = Math.floor(Math.random() * buscaminas.numColumnas);
 
-        if(buscaminas.aCamposMinas[fila][columna] != "B"){
+        if (buscaminas.aCamposMinas[fila][columna] != "B") {
             buscaminas.aCamposMinas[fila][columna] = "B";
         }
 
-        numMinasEsparcidas ++;
+        numMinasEsparcidas++;
     }
     console.log(buscaminas.aCamposMinas);
 }
 
-function contarMinasAlrededorCasilla(fila, columna){
+function contarMinasAlrededorCasilla(fila, columna) {
     let numeroMinasAlrededor = 0;
 
-    for(let zFila = fila-1; zFila <= fila+1; zFila++){
-        for(let zColumna = columna-1; zColumna <= columna+1; zColumna++){
-            if(zFila>-1 && zFila<buscaminas.numFilas && zColumna>-1 && zColumna<buscaminas.numColumnas){
-                if(buscaminas.aCamposMinas[zFila][zColumna] == "B"){
-                    numeroMinasAlrededor ++;
+    for (let zFila = fila - 1; zFila <= fila + 1; zFila++) {
+        for (let zColumna = columna - 1; zColumna <= columna + 1; zColumna++) {
+            if (zFila > -1 && zFila < buscaminas.numFilas && zColumna > -1 && zColumna < buscaminas.numColumnas) {
+                if (buscaminas.aCamposMinas[zFila][zColumna] == "B") {
+                    numeroMinasAlrededor++;
                 }
             }
         }
@@ -160,10 +167,10 @@ function contarMinasAlrededorCasilla(fila, columna){
     buscaminas.aCamposMinas[fila][columna] = numeroMinasAlrededor;
 }
 
-function esparcirNumeros(){
-    for(let i=0; i<buscaminas.numFilas; i++){
-        for(let j=0; j<buscaminas.numColumnas; j++){
-            if(buscaminas.aCamposMinas[i][j] != "B"){
+function esparcirNumeros() {
+    for (let i = 0; i < buscaminas.numFilas; i++) {
+        for (let j = 0; j < buscaminas.numColumnas; j++) {
+            if (buscaminas.aCamposMinas[i][j] != "B") {
                 contarMinasAlrededorCasilla(i, j);
             }
         }
@@ -171,3 +178,25 @@ function esparcirNumeros(){
 }
 
 window.onload = inicio();
+
+function resolverTablero(are_u_wining_son) {
+    for (let i = 0; i < buscaminas.numFilas; i++) {
+        for (let j = 0; j < buscaminas.numColumnas; j++) {
+            let casilla = document.querySelector("#f" + i + "_c" + j);
+            if (buscaminas.aCamposMinas[i][j] == "B" && (!casilla.classList.contains("destapada") && !casilla.classList.contains("fa-flag"))) {
+                casilla.innerHTML = "";
+                casilla.classList.add("fas");
+                casilla.classList.add("fa-bomb");
+                casilla.classList.add("sinmaracar");
+            } else if (buscaminas.aCamposMinas[i][j] == "B" && casilla.classList.contains("fa-flag")) {
+                casilla.classList.add("correct");
+                casilla.removeEventListener("contextmenu", marcar);
+                casilla.removeEventListener("click", destapar);
+            } else {
+                casilla.removeEventListener("contextmenu", marcar);
+                casilla.removeEventListener("click", destapar);
+            }
+        }
+    }
+    (are_u_wining_son) ? alert("Ganaste"): alert("Perdiste");
+}
